dbb1671905cee9c7e4791b40ca24442e4c9e09da
jainandhinis <jainandhinis@gmail.com>
2020-12-30 18:15:08 -0600

    solved day6 prob2

--- Day6/Day6Prob2/Program.cs
+++ Day6/Day6Prob2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day6Prob2
 {
@@ -11,13 +12,14 @@ namespace Day6Prob2
             Console.WriteLine("Hello World!-6.2");
             Program ps = new Program();
             var questionsCount = ps.CalculateNoQuestions();
-            System.Console.WriteLine($"Final questionsCount: {questionsCount}"); //6911
+            System.Console.WriteLine($"Final questionsCount: {questionsCount}"); //3473
         }
         int CalculateNoQuestions()
         {
             var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName,"input.txt");
             var questionsCount = 0;
-            var questions = new SortedSet<char>();
+            var questionsPerPerson = new SortedSet<char>();
+            var groupsResponse = new List<SortedSet<char>>();
             if (File.Exists(path))
             {
                 string[] lines = System.IO.File.ReadAllLines(path);
@@ -27,22 +29,33 @@ namespace Day6Prob2
                     var line = line1.Trim();
                     if (line == null || line.Length == 0)
                     {
-                        questionsCount += questions.Count;
-                        questions.Clear();
-                        questions = new SortedSet<char>();
+                        questionsCount += CountValidAnswers(groupsResponse);
+                        //move to next group
+                        groupsResponse = new List<SortedSet<char>>();
                         continue;
                     }
                     foreach(var c in line.ToCharArray())
                     {
-                        questions.Add(c);
+                        questionsPerPerson.Add(c);
                     }
-                    
+                    groupsResponse.Add(questionsPerPerson);
+                    //move to next person's response in same group
+                    questionsPerPerson = new SortedSet<char>();
+
                 }
-                //last group
-                questionsCount += questions.Count;
+                //calculate valid answers for last group
+                questionsCount += CountValidAnswers(groupsResponse);
             }
             Console.WriteLine($"returning count {questionsCount}");
             return questionsCount;
+        }
+
+        private int CountValidAnswers(List<SortedSet<char>> groupsResponse)
+        {
+            var peopleCount = groupsResponse.Count;
+            var answers = groupsResponse.SelectMany(c => c).GroupBy(c => c).Where(c=>c.Count()==peopleCount).Select(c=>c.Key);
+            Console.WriteLine($"returning {answers.Count()}");
+            return answers.Count();
         }
     }
 }
